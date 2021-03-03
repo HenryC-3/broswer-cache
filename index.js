7d62e0d279be72e42c73be254f507d01022d096c
HenryC-3 <2324375439@qq.com>
2021-03-03 18:55:58 +0800

    ETag实践

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const koa = require("koa");
 const static = require("koa-static");
 const app = new koa();
 const conditional = require("koa-conditional-get");
+const etag = require("koa-etag");
 
 app.use(conditional());
+app.use(etag());
 app.use(
 	// 可访问项目目录下的所有文件
 	static(".", {
